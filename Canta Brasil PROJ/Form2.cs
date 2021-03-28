0572d6f56679d7ec72b7dee8c52b932814067338
Klaus Vitor Marques Bighi <56765311+KlausBighi@users.noreply.github.com>
2021-03-28 11:54:16 -0300

    Add files via upload

---
+++ Canta Brasil PROJ/Form2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Canta_Brasil_PROJ
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+    }
+}
